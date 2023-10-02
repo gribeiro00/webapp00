e8b5f38e1c6f4ae1b80a3aa62c30b999e87e79f8
gribeiro00 <146739373+gribeiro00@users.noreply.github.com>
2023-10-02 10:55:37 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,7 +4,7 @@
 import streamlit as st
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Prof. Massaki Igarashi")
+st.title("gribeirodossantos00@gmail.com")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("Teste de Cabeçalho")
